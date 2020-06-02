f78ad11ffca7ad99d7045199bbecb1a793be8ca6
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-02 15:31:27 +0530

    Added: Partime Timme Salary

--- empWageComp.sh
+++ empWageComp.sh
@@ -2,14 +2,15 @@
 EMP_RATE_PER_HR=20;
 
 empCheck=$(($RANDOM%2));
+empCheck=$(($RANDOM%3));
 if [ $empCheck -eq 1 ]
 then
-	echo "present"
+	empHrs=4
+elif [ $empCheck -eq 2 ]
+then
 	empHrs=8
 else
-	echo "Absent"
 	empHrs=0
 fi
-
 totalSalary=$(($empHrs*$EMP_RATE_PER_HR))
 echo $totalSalary
