f63d8eb8b3301ce3445376d956daa7294dbeb207
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-02 16:44:42 +0530

    Added: Function to calculate working hours

--- empWageComp.sh
+++ empWageComp.sh
@@ -9,21 +9,28 @@ MAX_HRS_IN_MONTH=100;
 totalEmpHrs=0;
 totalWorkingDays=0;
 
+function getWorkHrs(){
+local empCheck=$1
+case $empCheck in
+  $IS_FULL_TIME)
+      empHrs=8
+         ;;
+  $IS_PART_TIME)
+       empHrs=4
+   ;;
+  *)
+    empHrs=0
+        ;;
+esac
+echo $empHrs
+}
+
+
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
 	empCheck=$(($RANDOM%3));
-	case $empCheck in
-  		$IS_FULL_TIME)
-      			empHrs=8
-         		;;
-  		$IS_PART_TIME)
-       			empHrs=4
-   			;;
-  		*)
-    			empHrs=0
-        		;;
-	esac
+	empHrs=$( getWorkHrs $empCheck )
 	totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
 done
 totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
