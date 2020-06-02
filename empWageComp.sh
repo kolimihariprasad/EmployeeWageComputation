c7d1f41466ae75a52b21753cc22ed490aa1a6761
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-02 15:37:56 +0530

    Added: Switch Case To Compute

--- empWageComp.sh
+++ empWageComp.sh
@@ -1,7 +1,8 @@
 # CONSTATNTS FOR THE PROGRAM
 EMP_RATE_PER_HR=20;
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
 
-empCheck=$(($RANDOM%2));
 empCheck=$(($RANDOM%3));
 if [ $empCheck -eq 1 ]
 then
@@ -12,5 +13,18 @@ then
 else
 	empHrs=0
 fi
+
+case $empCheck in
+  $IS_FULL_TIME)
+      empHrs=8
+         ;;
+  $IS_PART_TIME)
+       empHrs=4
+   ;;
+  *)
+    empHrs=0
+        ;;
+esac
+
 totalSalary=$(($empHrs*$EMP_RATE_PER_HR))
 echo $totalSalary
