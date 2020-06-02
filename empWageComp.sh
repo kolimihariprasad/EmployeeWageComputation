5d5aeab20c8e857c3b4739dda2f8e8ff734d5194
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-02 16:37:31 +0530

    Added: No of Working hours condition

--- empWageComp.sh
+++ empWageComp.sh
@@ -3,12 +3,13 @@ EMP_RATE_PER_HR=20;
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
 NUM_WORKING_DAYS=20;
+MAX_HRS_IN_MONTH=100;
 
 # VARIABLES
 totalEmpHrs=0;
 totalWorkingDays=0;
 
-while [[ $totalWorkingDays -le $NUM_WORKING_DAYS ]]
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
 	empCheck=$(($RANDOM%3));
