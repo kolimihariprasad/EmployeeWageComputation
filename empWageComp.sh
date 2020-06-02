14efcdf41985dcbd08cab52d080775e84b350c20
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-02 17:39:59 +0530

    Added: Diictionaries to store and display day, daily wage and total wage

--- empWageComp.sh
+++ empWageComp.sh
@@ -1,30 +1,37 @@
 # CONSTATNTS FOR THE PROGRAM
-EMP_RATE_PER_HR=20;
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
-NUM_WORKING_DAYS=20;
 MAX_HRS_IN_MONTH=100;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
 # VARIABLES
 totalEmpHrs=0;
 totalWorkingDays=0;
+salary=0;
+
+declare -A dailyWage;
+declare -A totalwage;
+declare -A dayno;
 
 function getWorkHrs(){
-local empCheck=$1
-case $empCheck in
-  $IS_FULL_TIME)
-      empHrs=8
-         ;;
-  $IS_PART_TIME)
-       empHrs=4
-   ;;
-  *)
-    empHrs=0
-        ;;
-esac
-echo $empHrs
+	local empCheck=$1
+	case $empCheck in
+		$IS_FULL_TIME)
+			empHrs=8
+			;;
+		$IS_PART_TIME)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+	echo $empHrs
 }
 
+echo "Welcome to the daily wage computation program"
+echo "Day no	Daily Wage	Total Wage"
 
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
@@ -33,8 +40,13 @@ do
 	empHrs=$( getWorkHrs $empCheck )
 	totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
 	dailyWage[$totalWorkingDays]=$(( $empHrs*$EMP_RATE_PER_HR ))
+	dayno[$totalWorkingDays]="Day "$totalWorkingDays
+	salary=$(($salary+($empHrs*$EMP_RATE_PER_HR) ))
+	totalwage[$totalWorkingDays]=$salary
 done
-totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
-echo $totalSalary
-echo ${dailyWage[@]}
-echo ${!dailyWage[@]}
+
+for((i=1;i<=$totalWorkingDays;i++))
+do
+	echo -e ${dayno[$i]}'\t'${dailyWage[$i]}'\t\t'${totalwage[$i]}
+done
+
