3674d51379a2839c58bd555d0a607be1148c525a
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-02 15:26:48 +0530

    Added: Calculating Daily Emp Wage

--- empWageComp.sh
+++ empWageComp.sh
@@ -1,7 +1,15 @@
-check=`shuf -i 0-1 -n 1`
-if [ $check -eq 1 ]
+# CONSTATNTS FOR THE PROGRAM
+EMP_RATE_PER_HR=20;
+
+empCheck=$(($RANDOM%2));
+if [ $empCheck -eq 1 ]
 then
-	echo "Present"
+	echo "present"
+	empHrs=8
 else
 	echo "Absent"
+	empHrs=0
 fi
+
+totalSalary=$(($empHrs*$EMP_RATE_PER_HR))
+echo $totalSalary
